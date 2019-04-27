1e8352610d3ada8276faa2274b6bd246d8609bab
Brian Burton <brian@burton-computer.com>
2019-04-27 18:38:50 -0400

    Refactoring.

--- node.go
+++ node.go
@@ -10,37 +10,12 @@ func emptyNode() *node {
 	return &node{}
 }
 
-func indexForHash(hashCode HashCode) int {
-	return int(hashCode & 0x0f)
-}
-
-func (this *node) getChild(index int) *node {
-	if this.children == nil {
-		return nil
-	} else {
-		return this.children[index]
-	}
-}
-
-func (this *node) updatedChildren(index int, child *node) []*node {
-	children := make([]*node, 32)
-	if this.children != nil {
-		copy(children, this.children)
-	}
-	children[index] = child
-	return children
-}
-
 func (this *node) assign(hashCode HashCode, key Object, value Object, equals EqualsFunc) *node {
 	if hashCode == 0 {
-		var newNode = *this
 		if this.key == nil {
-			newNode.key = key
-			newNode.value = value
-			return &newNode
+			return this.setKeyAndValue(key, value)
 		} else if equals(this.key, key) {
-			newNode.value = value
-			return &newNode
+			return this.setValue(value)
 		} else {
 			panic("hash collisions not yet supported")
 		}
@@ -51,10 +26,7 @@ func (this *node) assign(hashCode HashCode, key Object, value Object, equals Equ
 			oldChild = emptyNode()
 		}
 		newChild := oldChild.assign(hashCode>>5, key, value, equals)
-
-		newNode := *this
-		newNode.children = this.updatedChildren(index, newChild)
-		return &newNode
+		return this.setChild(index, newChild)
 	}
 }
 
@@ -76,6 +48,48 @@ func (this *node) get(hashCode HashCode, key Object, equals EqualsFunc) Object {
 	}
 }
 
+func (this *node) delete(hashCode HashCode, key Object, equals EqualsFunc) *node {
+	if hashCode == 0 {
+		if this.key == nil || !equals(this.key, key) {
+			panic("hash collisions not yet supported")
+		} else if this.childCount() == 0 {
+			return nil
+		} else {
+			return this.setKeyAndValue(nil, nil)
+		}
+	} else {
+		index := indexForHash(hashCode)
+		oldChild := this.getChild(index)
+		if oldChild == nil {
+			return this
+		} else {
+			newChild := oldChild.delete(hashCode>>5, key, equals)
+			if newChild == nil && this.key == nil && this.childCount() == 1 {
+				return nil
+			} else {
+				return this.setChild(index, newChild)
+			}
+		}
+	}
+}
+
+func indexForHash(hashCode HashCode) int {
+	return int(hashCode & 0x0f)
+}
+
+func (this *node) setValue(value Object) *node {
+	newNode := *this
+	newNode.value = value
+	return &newNode
+}
+
+func (this *node) setKeyAndValue(key Object, value Object) *node {
+	newNode := *this
+	newNode.key = key
+	newNode.value = value
+	return &newNode
+}
+
 func (this *node) childCount() int {
 	if this.children == nil {
 		return 0
@@ -89,32 +103,20 @@ func (this *node) childCount() int {
 	return count
 }
 
-func (this *node) delete(hashCode HashCode, key Object, equals EqualsFunc) *node {
-	if hashCode == 0 {
-		if this.key == nil || !equals(this.key, key) {
-			panic("hash collisions not yet supported")
-		} else if this.childCount() == 0 {
-			return nil
-		} else {
-			newNode := *this
-			newNode.key = nil
-			newNode.value = nil
-			return &newNode
-		}
+func (this *node) getChild(index int) *node {
+	if this.children == nil {
+		return nil
 	} else {
-		index := indexForHash(hashCode)
-		oldChild := this.getChild(index)
-		if oldChild == nil {
-			return this
-		} else {
-			newChild := oldChild.delete(hashCode>>5, key, equals)
-			if newChild == nil && this.key == nil && this.childCount() == 1 {
-				return nil
-			} else {
-				newNode := *this
-				newNode.children = this.updatedChildren(index, newChild)
-				return &newNode
-			}
-		}
+		return this.children[index]
 	}
+}
+
+func (this *node) setChild(index int, child *node) *node {
+	newNode := *this
+	newNode.children = make([]*node, 32)
+	if this.children != nil {
+		copy(newNode.children, this.children)
+	}
+	newNode.children[index] = child
+	return &newNode
 }
